c9440dda2e6f00c657fd4a2f8be40fc34864478f
林霁雷 <linjilei@qq.com>
2017-04-01 21:00:14 +0800

    vue emitter

--- vue/0.10.0-rc/emitter.js
+++ vue/0.10.0-rc/emitter.js
@@ -1,17 +1,32 @@
+/**
+ * 观察者
+ */
 function Emitter () {
+    // 上下文
     this._ctx = this
 }
 
 var EmitterProto = Emitter.prototype,
     slice = [].slice
-
+/**
+ * 监听事件
+ * 将监听 event.  并且将回调传入队列
+ * _cbs 是一个私有属性. 类型为对象.
+ * 包含各种事件的 数组.
+ */
 EmitterProto.on = function(event, fn){
     this._cbs = this._cbs || {}
     ;(this._cbs[event] = this._cbs[event] || [])
         .push(fn)
     return this
 }
-
+/**
+ * 监听一次事件
+ * 监听 event, 执行一次后. 取消监听.
+ * 通过将原先的 回调进行一次封装.
+ * 执行该封装后的方法后, 取消监听并执行原先的回调函数.
+ * 达到监听一次的效果.
+ */
 Emitter.prototype.once = function(event, fn){
     var self = this
     this._cbs = this._cbs || {}
@@ -25,27 +40,42 @@ Emitter.prototype.once = function(event, fn){
     this.on(event, on)
     return this
 }
-
+/**
+ * 取消监听.
+ * 可取消所有的监听对象, 或取消某个事件, 或从某个事件里取消某个回调
+ */
 Emitter.prototype.off = function(event, fn){
     this._cbs = this._cbs || {}
 
     // all
+    /**
+     * 如果传入的值为空. 即取消全部监听. 直接给_cbs 复制一个全新的对象.
+     */
     if (!arguments.length) {
         this._cbs = {}
         return this
     }
 
     // specific event
+    // 具体事件. 如果该事件为空. 则直接返回
     var callbacks = this._cbs[event]
     if (!callbacks) return this
 
     // remove all handlers
+    /**
+     * 当传入值只有一个时, 则取消该监听的 event 所有的方法.
+     * 即直接删除该监听的属性值
+     */
     if (arguments.length === 1) {
         delete this._cbs[event]
         return this
     }
 
     // remove specific handler
+    /**
+     * 移除具体的事件. 循环查找
+     * 若回调方法引用一致. 则通过 splice 删除该回调
+     */
     var cb
     for (var i = 0; i < callbacks.length; i++) {
         cb = callbacks[i]
@@ -56,7 +86,10 @@ Emitter.prototype.off = function(event, fn){
     }
     return this
 }
-
+/**
+ * 触发 触发event事件
+ * 直接读取私有_cbs对象, 循环并触发该队列
+ */
 Emitter.prototype.emit = function(event){
     this._cbs = this._cbs || {}
     var args = slice.call(arguments, 1),
--- vue/0.10.0-rc/observer.js
+++ vue/0.10.0-rc/observer.js
@@ -1,4 +1,7 @@
 /* jshint proto:true */
+/**
+ * 将一个对象转为一个 可观察的 对象
+ */
 
 var Emitter  = require('./emitter'),
     utils    = require('./utils'),
@@ -13,17 +16,27 @@ var Emitter  = require('./emitter'),
     // define methods as inenumerable if __proto__ is present,
     // otherwise enumerable so we can loop through and manually
     // attach to array instances
+    // 当在IE条件下时, 没有 __proto__. 则通过该方式去判断是否拥有.
+    // 以便之后好做判断兼容.
     hasProto = ({}).__proto__,
     // lazy load
     ViewModel
 
 // Array Mutation Handlers & Augmentations ------------------------------------
-
+/**
+ * 处理数组. 将数组转为可观察的数组
+ */
 // The proxy prototype to replace the __proto__ of
 // an observed array
+// 代理 数组的原型, 来代替 __proto__
 var ArrayProxy = Object.create(Array.prototype)
 
 // intercept mutation methods
+/**
+ * 拦截数组以下方式,
+ * 使其具有观察的能力,
+ * 通过以下方式去改变数组时, 都将会触发更新.
+ */
 ;[
     'push',
     'pop',
@@ -34,11 +47,13 @@ var ArrayProxy = Object.create(Array.prototype)
     'reverse'
 ].forEach(watchMutation)
 
+// 通过def 传入保护性的属性.
 // Augment the ArrayProxy with convenience methods
+// 设置值, 返回设置的值
 def(ArrayProxy, '$set', function (index, data) {
     return this.splice(index, 1, data)[0]
 }, !hasProto)
-
+// 删除值, 返回被删除的值
 def(ArrayProxy, '$remove', function (index) {
     if (typeof index !== 'number') {
         index = this.indexOf(index)
@@ -52,8 +67,13 @@ def(ArrayProxy, '$remove', function (index) {
  *  Intercep a mutation event so we can emit the mutation info.
  *  we also analyze what elements are added/removed and link/unlink
  *  them with the parent Array.
+ * 分析数组的方法, 使得数组在改变的时候能即时发出 被改变的通知.
+ * 同时也将分析数组在添加或者删除时, 判断是否与 父数组连接
  */
 function watchMutation (method) {
+    /**
+     * 传入 数组原型. 方法名, 以及 方法.
+     */
     def(ArrayProxy, method, function () {
 
         var args = slice.call(arguments),
@@ -61,6 +81,7 @@ function watchMutation (method) {
             inserted, removed
 
         // determine new / removed elements
+        // 取得 插入的值或被删除的值
         if (method === 'push' || method === 'unshift') {
             inserted = args
         } else if (method === 'pop' || method === 'shift') {
@@ -71,10 +92,12 @@ function watchMutation (method) {
         }
         
         // link & unlink
+        // 将插入的值与dom连接. 移除的值则断开连接
         linkArrayElements(this, inserted)
         unlinkArrayElements(this, removed)
 
         // emit the mutation event
+        // 触发事件. 刷新页面
         this.__emitter__.emit('mutate', '', this, {
             method   : method,
             args     : args,
@@ -91,19 +114,25 @@ function watchMutation (method) {
 /**
  *  Link new elements to an Array, so when they change
  *  and emit events, the owner Array can be notified.
+ * 新元素推入数组后, 将更新本身的观察者. 以便之后触发数据更新后 更新DOM
+ * arr 为 数组. items 为 新插入的数组
  */
 function linkArrayElements (arr, items) {
     if (items) {
         var i = items.length, item, owners
+        // 循环遍历新插入的值
         while (i--) {
             item = items[i]
+            // 判断是否需要观察该值
             if (isWatchable(item)) {
                 // if object is not converted for observing
                 // convert it...
+                // 如果该值需要被观察, 而本身却没有被观察, 则将其转换, 并且观察他.
                 if (!item.__emitter__) {
                     convert(item)
                     watch(item)
                 }
+                // TODO.
                 owners = item.__emitter__.owners
                 if (owners.indexOf(arr) < 0) {
                     owners.push(arr)
@@ -115,6 +144,7 @@ function linkArrayElements (arr, items) {
 
 /**
  *  Unlink removed elements from the ex-owner Array.
+ * 当值被删除后, 则将 __emitter__ 里的观察对象移除
  */
 function unlinkArrayElements (arr, items) {
     if (items) {
@@ -122,6 +152,7 @@ function unlinkArrayElements (arr, items) {
         while (i--) {
             item = items[i]
             if (item && item.__emitter__) {
+                // TODO
                 var owners = item.__emitter__.owners
                 if (owners) owners.splice(owners.indexOf(arr))
             }
@@ -130,7 +161,7 @@ function unlinkArrayElements (arr, items) {
 }
 
 // Object add/delete key augmentation -----------------------------------------
-
+// 对象代理...
 var ObjProxy = Object.create(Object.prototype)
 
 def(ObjProxy, '$add', function (key, val) {
@@ -153,6 +184,7 @@ def(ObjProxy, '$delete', function (key) {
 
 /**
  *  Check if a value is watchable
+ * 判断这个值是否需要观察
  */
 function isWatchable (obj) {
     ViewModel = ViewModel || require('./viewmodel')
@@ -162,6 +194,11 @@ function isWatchable (obj) {
 
 /**
  *  Convert an Object/Array to give it a change emitter.
+ * 将一个对象/数组转化为一个观察者.
+ * 利用def. 保证打印属性时 不会出现观察者.
+ * 通过给他添加 __emitter__ 属性, 赋值 一个全新的观察者对象
+ * 监听 set事件.以及 变更页面事件.
+ * 并且定义了owners 以及 values属性(TODO)
  */
 function convert (obj) {
     if (obj.__emitter__) return true
@@ -181,6 +218,7 @@ function convert (obj) {
 
 /**
  *  Propagate an array element's change to its owner arrays
+ * 当一个数组变化时, 广播数组下所有的子数组.
  */
 function propagateChange (obj) {
     var owners = obj.__emitter__.owners,
@@ -192,6 +230,7 @@ function propagateChange (obj) {
 
 /**
  *  Watch target based on its type
+ * // 观察对象. 观察分两种. Object 以及 Array
  */
 function watch (obj) {
     var type = typeOf(obj)
