b98995a894a168eb248840cb9cced5fac03c19ae
林霁雷 <linjilei@qq.com>
2017-04-05 13:12:42 +0800

    Vue main filters

--- vue/0.10.0-rc/filters.js
+++ vue/0.10.0-rc/filters.js
@@ -1,8 +1,13 @@
+/**
+ * 过滤
+ */
 var utils    = require('./utils'),
     get      = utils.get,
     slice    = [].slice,
     QUOTE_RE = /^'.*'$/
-
+/**
+ * 键盘别名
+ */
 var keyCodes = {
     enter    : 13,
     tab      : 9,
@@ -13,11 +18,14 @@ var keyCodes = {
     down     : 40,
     esc      : 27
 }
-
+/**
+ * Vue自带的过滤器
+ */
 var filters = module.exports = {
 
     /**
      *  'abc' => 'Abc'
+     * 驼峰
      */
     capitalize: function (value) {
         if (!value && value !== 0) return ''
@@ -27,6 +35,7 @@ var filters = module.exports = {
 
     /**
      *  'abc' => 'ABC'
+     * 小写转大写
      */
     uppercase: function (value) {
         return (value || value === 0)
@@ -36,6 +45,7 @@ var filters = module.exports = {
 
     /**
      *  'AbC' => 'abc'
+     * 大写转小写
      */
     lowercase: function (value) {
         return (value || value === 0)
@@ -45,6 +55,7 @@ var filters = module.exports = {
 
     /**
      *  12345 => $12,345.00
+     * 字符转货币
      */
     currency: function (value, sign) {
         if (!value && value !== 0) return ''
@@ -64,6 +75,9 @@ var filters = module.exports = {
      *  entry in the array.
      *
      *  e.g. ['single', 'double', 'triple', 'multiple']
+     * 复数形式. 接受值 和 复数单位参数.
+     * 自动在后面跟上 复数单位.
+     * TODO: 没怎么用过
      */
     pluralize: function (value) {
         var args = slice.call(arguments, 1)
@@ -75,6 +89,7 @@ var filters = module.exports = {
     /**
      *  A special filter that takes a handler function,
      *  wraps it so it only gets triggered on specific keypresses.
+     * 设置按键. 包装传入的回调, 判断触发的keyCode, 触发回调
      */
     key: function (handler, key) {
         if (!handler) return
@@ -88,11 +103,14 @@ var filters = module.exports = {
             }
         }
     },
-
+    /**
+     * 数组过滤
+     */
     filterBy: function (arr, searchKey, delimiter, dataKey) {
 
         // allow optional `in` delimiter
         // because why not
+        // v-for = "item in items" `in`即分隔符
         if (delimiter && delimiter !== 'in') {
             dataKey = delimiter
         }
@@ -106,6 +124,7 @@ var filters = module.exports = {
         dataKey = dataKey && (stripQuotes(dataKey) || get(this, dataKey))
 
         // convert object to array
+        // 对象转为数组后, 调用数组的 过滤方法
         if (!Array.isArray(arr)) {
             arr = utils.objectToArray(arr)
         }
@@ -117,18 +136,22 @@ var filters = module.exports = {
         })
 
     },
-
+    /**
+     * 数组排序
+     */
     orderBy: function (arr, sortKey, reverseKey) {
 
         var key = stripQuotes(sortKey) || get(this, sortKey)
         if (!key) return arr
 
         // convert object to array
+        // 对象转为数组
         if (!Array.isArray(arr)) {
             arr = utils.objectToArray(arr)
         }
 
         var order = 1
+        // 获取排序关键字
         if (reverseKey) {
             if (reverseKey === '-1') {
                 order = -1
@@ -141,6 +164,10 @@ var filters = module.exports = {
         }
 
         // sort on a copy to avoid mutating original array
+        /**
+         * 利用 slice() 来快速创建一个新的数组. 然后利用数组的排序来进行排序
+         * 与原先的数组不冲突
+         */
         return arr.slice().sort(function (a, b) {
             a = get(a, key)
             b = get(b, key)
@@ -152,9 +179,10 @@ var filters = module.exports = {
 }
 
 // Array filter helpers -------------------------------------------------------
-
+// 数组过滤助手方法
 /**
  *  String contain helper
+ * 判断是否包含字符串
  */
 function contains (val, search) {
     /* jshint eqeqeq: false */
@@ -172,6 +200,8 @@ function contains (val, search) {
 /**
  *  Test whether a string is in quotes,
  *  if yes return stripped string
+ * 去除引号.
+ * 判断字符串是否两边包含引号. 若有, 则去除引号.
  */
 function stripQuotes (str) {
     if (QUOTE_RE.test(str)) {
@@ -180,5 +210,6 @@ function stripQuotes (str) {
 }
 
 // mark computed filters
+// TODO: 标记可计算?
 filters.filterBy.computed = true
 filters.orderBy.computed = true
--- vue/0.10.0-rc/main.js
+++ vue/0.10.0-rc/main.js
@@ -1,3 +1,4 @@
+
 var config      = require('./config'),
     ViewModel   = require('./viewmodel'),
     utils       = require('./utils'),
@@ -6,9 +7,11 @@ var config      = require('./config'),
 
 // require these so Browserify can catch them
 // so they can be used in Vue.require
+// Browserify插件
 require('./observer')
 require('./transition')
 
+// Vue 全局资源
 ViewModel.options = config.globalAssets = {
     directives  : require('./directives'),
     filters     : require('./filters'),
@@ -19,6 +22,7 @@ ViewModel.options = config.globalAssets = {
 
 /**
  *  Expose asset registration methods
+ * 暴露资源的注册方法
  */
 assetTypes.forEach(function (type) {
     ViewModel[type] = function (id, value) {
@@ -28,10 +32,13 @@ assetTypes.forEach(function (type) {
         }
         if (!value) return hash[id]
         if (type === 'partial') {
+            // 创建DOM片段
             value = utils.toFragment(value)
         } else if (type === 'component') {
+            // 将对象转为 vue 组件
             value = utils.toConstructor(value)
         } else if (type === 'filter') {
+            // 判断过滤函数
             utils.checkFilter(value)
         }
         hash[id] = value
@@ -41,6 +48,7 @@ assetTypes.forEach(function (type) {
 
 /**
  *  Set config options
+ * 获取 设置 值
  */
 ViewModel.config = function (opts, val) {
     if (typeof opts === 'string') {
@@ -57,6 +65,7 @@ ViewModel.config = function (opts, val) {
 
 /**
  *  Expose an interface for plugins
+ * 安装插件 调用 plugin对象的 install来安装
  */
 ViewModel.use = function (plugin) {
     if (typeof plugin === 'string') {
@@ -82,23 +91,26 @@ ViewModel.use = function (plugin) {
 
 /**
  *  Expose internal modules for plugins
+ * 暴露 引用模块
  */
 ViewModel.require = function (path) {
     return require('./' + path)
 }
-
+// 添加方法
 ViewModel.extend = extend
 ViewModel.nextTick = utils.nextTick
 
 /**
  *  Expose the main ViewModel class
  *  and add extend method
+ * 添加 extend. 扩展组件方法
  */
 function extend (options) {
-
+    // 父vm
     var ParentVM = this
 
     // inherit options
+    // 继承并转换值
     options = inheritOptions(options, ParentVM.options, true)
     utils.processOptions(options)
 
@@ -114,6 +126,7 @@ function extend (options) {
     utils.defProtected(proto, 'constructor', ExtendedVM)
 
     // copy prototype props
+    // 复制方法
     var methods = options.methods
     if (methods) {
         for (var key in methods) {
@@ -132,11 +145,13 @@ function extend (options) {
     ExtendedVM.options = options
 
     // allow extended VM to add its own assets
+    // 扩展资源方法
     assetTypes.forEach(function (type) {
         ExtendedVM[type] = ViewModel[type]
     })
 
     // allow extended VM to use plugins
+    // 子组件 也能调用插件.
     ExtendedVM.use     = ViewModel.use
     ExtendedVM.require = ViewModel.require
 
@@ -155,6 +170,8 @@ function extend (options) {
  *
  *  `el` is an exception because it's not allowed as an
  *  extension option, but only as an instance option.
+ * 继承. 子组件 继承父组件.
+ * 将会继承 data vms directives partials属性.
  */
 function inheritOptions (child, parent, topLevel) {
     child = child || {}
@@ -167,6 +184,7 @@ function inheritOptions (child, parent, topLevel) {
             parentType = utils.typeOf(parentVal)
         if (topLevel && type === 'Function' && parentVal) {
             // merge hook functions into an array
+            // 合并 钩子 方法
             child[key] = [val]
             if (Array.isArray(parentVal)) {
                 child[key] = child[key].concat(parentVal)
@@ -179,9 +197,11 @@ function inheritOptions (child, parent, topLevel) {
             && !(parentVal instanceof ViewModel)
         ) {
             // merge toplevel object options
+
             child[key] = inheritOptions(val, parentVal)
         } else if (val === undefined) {
             // inherit if child doesn't override
+            // 值为空的时候直接引用父组件值
             child[key] = parentVal
         }
     }
