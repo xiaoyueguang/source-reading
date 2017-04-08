9fa1f6b259a3675d0f58f10b9429acaf799394b5
林霁雷 <linjilei@qq.com>
2017-04-08 12:40:10 +0800

    vue text-parse

--- vue/0.10.0-rc/text-parser.js
+++ vue/0.10.0-rc/text-parser.js
@@ -1,18 +1,28 @@
+/**
+ * 解析 dom属性里 或 dom文本里的字符
+ */
 var openChar  = '{',
     endChar   = '}',
     ESCAPE_RE  = /[-.*+?^${}()|[\]\/\\]/g,
     BINDING_RE = buildInterpolationRegex()
-
+/**
+ * 构建正则. 匹配 {{}} {{{}}}
+ */
 function buildInterpolationRegex () {
     var open = escapeRegex(openChar),
         end  = escapeRegex(endChar)
     return new RegExp(open + open + open + '?(.+?)' + end + '?' + end + end)
 }
-
+/**
+ * 
+ */
 function escapeRegex (str) {
     return str.replace(ESCAPE_RE, '\\$&')
 }
-
+/**
+ * 设置界定符.
+ * @param {*} delimiters 
+ */
 function setDelimiters (delimiters) {
     exports.delimiters = delimiters
     openChar = delimiters[0]
@@ -26,6 +36,14 @@ function setDelimiters (delimiters) {
  *  1. plain string
  *  2. object with key = binding key
  *  3. object with key & html = true
+ * 解析字符串. 返回 包含令牌的数组
+ * 令牌类型有
+ *   纯字符串
+ *   对象 界定符里的字符串传入到 key 里
+ *   对象 {{{}}} 如果是这种, 则 对象里的html 为true
+ *
+ * 例子: parse('{{msg}} msg: {{{msg}}}')
+ * [{"key":"msg","html":false}," msg: ",{"key":"msg","html":true}]
  */
 function parse (text) {
     if (!BINDING_RE.test(text)) return null
@@ -51,6 +69,8 @@ function parse (text) {
  *  return a Directive-friendly expression
  *
  *  e.g.  a {{b}} c  =>  "a " + b + " c"
+ * 从 dom 的属性里 解析. 返回一个对 指令友好的 表达式
+ * v-class = 'a {{b}} c' => "a " + b + " c"
  */
 function parseAttr (attr) {
     var tokens = parse(attr)
