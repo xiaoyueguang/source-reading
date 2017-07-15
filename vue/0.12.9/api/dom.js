622a476e172febef642f9e3b8291e6055387d964
linjilei <linjilei@qq.com>
2017-07-15 22:48:47 +0800

    vue dom

--- vue/0.12.9/api/dom.js
+++ vue/0.12.9/api/dom.js
@@ -1,3 +1,6 @@
+/**
+ * DOM 操作相关
+ */
 var _ = require('../util')
 var transition = require('../transition')
 
@@ -5,7 +8,8 @@ var transition = require('../transition')
  * Convenience on-instance nextTick. The callback is
  * auto-bound to the instance, and this avoids component
  * modules having to rely on the global Vue.
- *
+ * 异步回调. 因为界面刷新的时候是同步的
+ * 调用 异步方法即可实现 界面更新完成后再执行回调
  * @param {Function} fn
  */
 
@@ -15,7 +19,7 @@ exports.$nextTick = function (fn) {
 
 /**
  * Append instance to target
- *
+ * 将实例插入到目标 DOM 中. 为目标的最后个子节点
  * @param {Node} target
  * @param {Function} [cb]
  * @param {Boolean} [withTransition] - defaults to true
@@ -30,7 +34,7 @@ exports.$appendTo = function (target, cb, withTransition) {
 
 /**
  * Prepend instance to target
- *
+ * 将实例插入到目标 DOM 中. 为目标的第一个子节点
  * @param {Node} target
  * @param {Function} [cb]
  * @param {Boolean} [withTransition] - defaults to true
@@ -48,7 +52,7 @@ exports.$prependTo = function (target, cb, withTransition) {
 
 /**
  * Insert instance before target
- *
+ * 将实例添加到目标节点前
  * @param {Node} target
  * @param {Function} [cb]
  * @param {Boolean} [withTransition] - defaults to true
@@ -63,7 +67,7 @@ exports.$before = function (target, cb, withTransition) {
 
 /**
  * Insert instance after target
- *
+ * 将实例添加到目标节点后
  * @param {Node} target
  * @param {Function} [cb]
  * @param {Boolean} [withTransition] - defaults to true
@@ -81,7 +85,7 @@ exports.$after = function (target, cb, withTransition) {
 
 /**
  * Remove instance from DOM
- *
+ * 从节点中移除实例
  * @param {Function} [cb]
  * @param {Boolean} [withTransition] - defaults to true
  */
@@ -97,6 +101,7 @@ exports.$remove = function (cb, withTransition) {
   var op
   var self = this
   var realCb = function () {
+    // 触发生命钩子
     if (inDoc) self._callHook('detached')
     if (cb) cb()
   }
@@ -119,13 +124,13 @@ exports.$remove = function (cb, withTransition) {
 
 /**
  * Shared DOM insertion function.
- *
- * @param {Vue} vm
- * @param {Element} target
- * @param {Function} [cb]
- * @param {Boolean} [withTransition]
- * @param {Function} op1 - op for non-transition insert
- * @param {Function} op2 - op for transition insert
+ * DOM 插入方法.
+ * @param {Vue} vm 实例
+ * @param {Element} target 目标 DOM 节点
+ * @param {Function} [cb] 
+ * @param {Boolean} [withTransition] 是否启用过渡
+ * @param {Function} op1 - op for non-transition insert 无过渡插入 方法
+ * @param {Function} op2 - op for transition insert 有过渡插入 方法
  * @return vm
  */
 
@@ -151,6 +156,7 @@ function insert (vm, target, cb, withTransition, op1, op2) {
 }
 
 /**
+ * TODO:
  * Execute a transition operation on a fragment instance,
  * iterating through all its block nodes.
  *
@@ -174,7 +180,7 @@ function blockOp (vm, target, op, cb) {
 
 /**
  * Check for selectors
- *
+ * 查找 DOM 节点
  * @param {String|Element} el
  */
 
@@ -186,7 +192,7 @@ function query (el) {
 
 /**
  * Append operation that takes a callback.
- *
+ * 将 节点添加到 目标 节点下 并执行回调
  * @param {Node} el
  * @param {Node} target
  * @param {Vue} vm - unused
@@ -200,7 +206,7 @@ function append (el, target, vm, cb) {
 
 /**
  * InsertBefore operation that takes a callback.
- *
+ * 将 节点 添加到目标节点前, 并执行回调
  * @param {Node} el
  * @param {Node} target
  * @param {Vue} vm - unused
@@ -214,7 +220,7 @@ function before (el, target, vm, cb) {
 
 /**
  * Remove operation that takes a callback.
- *
+ * 移除节点, 并执行回调
  * @param {Node} el
  * @param {Vue} vm - unused
  * @param {Function} [cb]
